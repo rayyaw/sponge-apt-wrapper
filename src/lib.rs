c4f655b3ad1a13f8098591949d4fff737158d58c
rayyaw <rayyawspeedruns@gmail.com>
2024-03-03 17:19:34 -0600

    working on install api

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ static PLUGIN_NAME: &CStr = unsafe { CStr::from_bytes_with_nul_unchecked(b"apt\0
 pub extern "C" fn plugin_name() -> *const libc::c_char { PLUGIN_NAME.as_ptr() }
 
 #[no_mangle]
-pub extern "C" fn upgrade_all_api_version() -> i32 { 1 }
+pub extern "C" fn upgrade_all_api_version() -> u32 { 1 }
 
 #[no_mangle]
 pub extern "C" fn upgrade_all_api_v1() -> () {
@@ -19,6 +19,36 @@ pub extern "C" fn upgrade_all_api_v1() -> () {
         .expect("Error: Could not run 'apt upgrade'!");
 
     verify_exit_code(apt_upgrade);
+}
+
+#[no_mangle]
+pub extern "C" fn bulk_install_api_version () -> u32 { 1 }
+
+#[no_mangle]
+pub extern "C" fn bulk_install_api_v1(
+    packages: *const *const libc::c_char, num_packages: usize
+) -> bool {
+    let mut command_base = Command::new("apt");
+    let mut command = command_base.arg("install");
+    
+    for i in 0..num_packages { // FIXME - this is not getting the correct value (segfault)
+        eprintln!("{i}");
+        let package_c_str = unsafe { *packages.offset(i as isize) };
+        let package = unsafe { CStr::from_ptr(package_c_str).to_string_lossy() };
+        let package_str = package.as_ref();
+        command = command.arg(package_str);
+    }
+
+    let command_string = format!("B {:?}", command);
+    
+    // Print the command string to stderr
+    eprintln!("{}", command_string);
+
+    if let Ok(_) = command.output() {
+        true
+    } else {
+        false
+    }
 }
 
 fn get_upgradable_packages() -> () {
